dd8d2cfbcd51605fc2a9375f2c458733ee2310dd
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-03-28 16:00:29 +0000

    sdlt calculations seem to be correct

--- cls_helper_sdlt.py
+++ cls_helper_sdlt.py
@@ -1,5 +1,7 @@
 from enum import Enum, auto
 
+# https://www.tax.service.gov.uk/calculate-stamp-duty-land-tax/
+
 
 class SDLT_BuyerTypes(Enum):
     FIRST_TIME_BUYER = auto()
@@ -12,36 +14,38 @@ class SDLTCalculator:
     SDLT_Rates = {
         SDLT_BuyerTypes.FIRST_TIME_BUYER: {
             "rates": {
-                "band_1": {"limit": 300000, "rate": 0},
-                "band_2": {"limit": 500000, "rate": 5},
+                "band_1": {"upper_bound": 300000, "rate": 0},
+                "band_2": {"upper_bound": 500000, "rate": 5},
             },
+            "max_price": 500000,
         },
         SDLT_BuyerTypes.NON_FIRST_TIME_BUYER: {
             "rates": {
-                "band_1": {"limit": 125000, "rate": 0},
-                "band_2": {"limit": 250000, "rate": 2},
-                "band_3": {"limit": 925000, "rate": 5},
-                "band_4": {"limit": 1500000, "rate": 10},
-                "band_5": {"limit": 50000000, "rate": 12},
+                "band_1": {"upper_bound": 125000, "rate": 0},
+                "band_2": {"upper_bound": 250000, "rate": 2},
+                "band_3": {"upper_bound": 925000, "rate": 5},
+                "band_4": {"upper_bound": 1500000, "rate": 10},
+                "band_5": {"upper_bound": 9999999, "rate": 12},
             },
         },
         SDLT_BuyerTypes.SECOND_HOME_BUYER: {
             "rates": {
-                "band_1": {"limit": 125000, "rate": 5},
-                "band_2": {"limit": 250000, "rate": 7},
-                "band_3": {"limit": 925000, "rate": 10},
-                "band_4": {"limit": 1500000, "rate": 15},
-                "band_5": {"limit": 50000000, "rate": 17},
+                "band_1": {"upper_bound": 125000, "rate": 5},
+                "band_2": {"upper_bound": 250000, "rate": 7},
+                "band_3": {"upper_bound": 925000, "rate": 10},
+                "band_4": {"upper_bound": 1500000, "rate": 12},
+                "band_5": {"upper_bound": 9999999, "rate": 17},
             },
         },
         SDLT_BuyerTypes.CORPORATE_BUYER: {
             "rates": {
-                "band_1": {"limit": 125000, "rate": 0},
-                "band_2": {"limit": 250000, "rate": 2},
-                "band_3": {"limit": 925000, "rate": 5},
-                "band_4": {"limit": 1500000, "rate": 10},
-                "band_5": {"limit": 50000000, "rate": 12},
+                "band_1": {"upper_bound": 125000, "rate": 5},
+                "band_2": {"upper_bound": 250000, "rate": 7},
+                "band_3": {"upper_bound": 925000, "rate": 10},
+                "band_4": {"upper_bound": 1500000, "rate": 12},
+                "band_5": {"upper_bound": 9999999, "rate": 17},
             },
+            "zero_due_price": 40000,
         },
     }
     """
@@ -51,45 +55,46 @@ class SDLTCalculator:
     def __init__(self, buyer_type=SDLT_BuyerTypes.NON_FIRST_TIME_BUYER):
         self.buyer_type = buyer_type
 
-    def calculate_sdlt(self, property_price):
+    def calculate_sdlt(self, buyer_type, property_price):
         """
         Calculate the SDLT based on the property price.
 
         :param property_price: The price of the property
         :return: The amount of SDLT owed
         """
+
+        if buyer_type is SDLT_BuyerTypes.FIRST_TIME_BUYER:
+            if property_price > self.SDLT_Rates[buyer_type]["max_price"]:
+                return self._calculate_sdlt(
+                    SDLT_BuyerTypes.NON_FIRST_TIME_BUYER, property_price
+                )
+
+        if buyer_type is SDLT_BuyerTypes.CORPORATE_BUYER:
+            if property_price < self.SDLT_Rates[buyer_type]["zero_due_price"]:
+                return 0
+
         sdlt = 0
-        rates = self.SDLT_Rates[self.buyer_type]["rates"]
-        rates = dict(reversed(rates.items()))
-        for rate_details in rates.items():
-            band = rate_details[1]
-            limit = band["limit"]
-            if property_price > limit:
-                slice = property_price - limit
-                print(f"slice: {slice}")
-                print(f"Band limit: {band['limit']}")
-                print(f"Band rate: {band['rate']}")
-                rate = band["rate"]
-                sdlt += slice * (rate / 100)
-                print(f"SDLT so far: {sdlt}")
-                property_price = limit
+        lower_bound = 0
+        rates = self.SDLT_Rates[buyer_type]["rates"]
+
+        # Sort bands by upper_bound (ascending order)
+        sorted_bands = sorted(rates.values(), key=lambda x: x["upper_bound"])
+        # breakpoint()
+
+        for band in sorted_bands:
+            upper_bound = band["upper_bound"]
+            rate = band["rate"]
+            if property_price > lower_bound:
+                # Calculate the taxable amount for this band
+                # Ensure we don't go above the property price
+                if property_price < upper_bound:
+                    upper_bound = property_price
+                taxable_amount = upper_bound - lower_bound
+                sdlt_due = taxable_amount * (rate / 100)
+                sdlt += sdlt_due
+            lower_bound = upper_bound
 
         return sdlt
-
-    def set_price(self, price):
-        """
-        Set the property price.
-
-        :param price: The price of the property
-        """
-        self.price = price
-
-    def get_price(self):
-        """
-        Get the property price.
-        :return: The price of the property
-        """
-        return self.price
 
     def print_calculations(self, price):
         """
@@ -98,15 +103,43 @@ class SDLTCalculator:
         :param price: The price of the property
         :return: The SDLT amount
         """
+        print(f"Price: £{price:,}")
         for buyer_type in SDLT_BuyerTypes:
-            print(f"Calculating SDLT for {buyer_type.name.replace('_', ' ').title()}")
-            self.buyer_type = buyer_type
-            self.set_price(price)
-            sdlt = self.calculate_sdlt(price)
-            print(f"SDLT for a property worth £{price:,}: £{sdlt:,.2f}")
+            title = f"{buyer_type.name.replace('_', ' ').title()}"
+            sdlt = self.calculate_sdlt(buyer_type, price)
+            print(f"SDLT: £{sdlt:>10,.2f} for a {title}")
+
+    def print_rates(self):
+        for buyer_type in SDLT_BuyerTypes:
+            print(f"\n" + "=" * 40)
+            title = f"{buyer_type.name.replace('_', ' ').title()}"
+            rates = self.SDLT_Rates[buyer_type]["rates"]
+            sorted_bands = sorted(rates.items(), key=lambda x: x[1]["upper_bound"])
+            last_band_index = len(sorted_bands) - 1
+
+            for i, (band, details) in enumerate(sorted_bands):
+                # Get the details for the current band
+                upper_bound = details["upper_bound"]
+                rate = details["rate"]
+
+                if i == 0:
+                    print(f"{title} {band}: up to £{upper_bound:,} at {rate}%")
+                elif i == last_band_index:
+                    print(
+                        f"{title} {band}: above £{sorted_bands[i-1][1]['upper_bound']:,} at {rate}% (last band)"
+                    )
+                else:
+                    print(f"{title} {band}: then up to £{upper_bound:,} at {rate}%")
+
+        print("\n" + "=" * 40)
+        print(
+            "Note: SDLT rates are subject to change. Always check the latest government guidelines."
+        )
+        print("=" * 40)
 
 
 # Example usage
-price = 280000
+price = 310000
 sdlt_calculator = SDLTCalculator()
-sdlt = sdlt_calculator.print_calculations(price)
+sdlt_calculator.print_calculations(price)
+sdlt_calculator.print_rates()
